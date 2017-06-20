635d603be90b9a19f6f42957a1053bdce5ad9bf6
toloveru <mdr1996@live.be>
2017-06-20 22:24:35 +0200

    Added automatic version detection and removed all Python dependencies

--- KaliFixer.sh
+++ KaliFixer.sh
@@ -1,141 +1,83 @@
-#! /usr/bin/bash
+#!/bin/bash
 
-function The_Kali_Rolling_Repository() {
-    python3 - <<END
-
-from bs4 import BeautifulSoup
-import urllib.request
-
-kaliwebsite = "https://paste.ubuntu.com/24907580/"
-
-with urllib.request.urlopen(kaliwebsite) as html:
-    resp = html.read()
-
-bt = BeautifulSoup(resp, 'html.parser')
-
-s = bt.pre.next.next.next.string
-
-try:
-    with open("/etc/apt/sources.list", "w") as file:
-        file.write(s)
-except Exception as ex:
-    print(ex)
-    print("[!] Run Script as a root user to upgrade repos and fix the error of package")
-END
-}
-
-function Retired_Kali_sana_2_0_Repositories {
-    python3 - <<END
-
-from bs4 import BeautifulSoup
-import urllib.request
-
-kaliwebsite = "https://paste.ubuntu.com/24907595/"
-
-with urllib.request.urlopen(kaliwebsite) as html:
-    resp = html.read()
-
-bt = BeautifulSoup(resp, 'html.parser')
-
-s = bt.pre.next.next.next.string
-
-try:
-    with open("/etc/apt/sources.list", "w") as file:
-        file.write(s)
-except Exception as ex:
-    print(ex)
-    print("[!] Run Script as a root user to upgrade repos and fix the error of package")
-END
-}
-
-function Retired_Kali_moto_1_0_Repositories {
-    python3 - <<END
-
-from bs4 import BeautifulSoup
-import urllib.request
-
-kaliwebsite = "https://paste.ubuntu.com/24907611/"
-
-with urllib.request.urlopen(kaliwebsite) as html:
-    resp = html.read()
-
-bt = BeautifulSoup(resp, 'html.parser')
-
-s = bt.pre.next.next.next.string
-
-try:
-    with open("/etc/apt/sources.list", "w") as file:
-        file.write(s)
-except Exception as ex:
-    print(ex)
-    print("[!] Run Script as a root user to upgrade repos and fix the error of package")
-END
-}
+if [ $EUID != 0 ]
+then
+	printf "Must run as root!\n"
+	exit 1
+fi
 
 echo """
 +---------------------------------------------------------------------------------+
 |                            Author:- Techwebspot                                 |
 +---------------------------------------------------------------------------------+
-                       _  __     _ _                         
-                      | |/ /    | (_)                     
-                      | ' / __ _| |_|     
+                       _  __     _ _
+                      | |/ /    | (_)
+                      | ' / __ _| |_|
                       |  < / _\` | | |
-                      | . \ (_| | | | 
-                      |_|\_\__,_|_|_|   _____             
+                      | . \ (_| | | |
+                      |_|\_\__,_|_|_|   _____
                                        / __(_)_ _____ ____
                                       / _// /\ \ / -_) __/
-                                     /_/ /_//_\_\\__/_/   
-    -------------------------------------------------------------------------
-     Note:- Run Script with root user for update repos and fix package error
-    -------------------------------------------------------------------------
+                                     /_/ /_//_\_\\__/_/
+
 """
 
-echo "Choose your Kali Linux version from below index number:-"
-echo "1. The Kali Rolling"
-echo "2. Retired Kali sana (2.0)"
-echo "3. Retired Kali moto (1.0)"
-echo "Press Enter if you don't know which option you want to select"
+function kali-rolling(){
+	printf "[+] Using Kali rolling.\n"
+	if [ -w /etc/apt/sources.list ]
+	then
+		printf "[+] Updating repos, please wait..\n"
+		cat <<-EOF > /etc/apt/sources.list
+		deb http://http.kali.org/kali kali-rolling main contrib non-free
+		# For source package access, uncomment the following line
+		# deb-src http://http.kali.org/kali kali-rolling main contrib non-free
+		EOF
+	fi
+}
+function kali-sana(){
+	printf "[+] Using Kali Sana.\n"
+	if [ -w /etc/apt/sources.list ]
+	then
+		printf "[+] Updating repos, please wait..\n"
+		cat <<-EOF > /etc/apt/sources.list
+		deb http://old.kali.org/kali sana main non-free contrib
+		# For source package access, uncomment the following line
+		# deb-src http://old.kali.org/kali sana main non-free contrib
+		EOF
+	fi
+}
+function kali-moto(){
+	printf "[+] Using Kali Moto.\n"
+	if [ -w /etc/apt/sources.list ]
+	then
+		printf "[+] Updating repos, please wait..\n"
+		cat <<-EOF > /etc/apt/sources.list
+		deb http://old.kali.org/kali moto main non-free contrib
+		# For source package access, uncomment the following line
+		# deb-src http://old.kali.org/kali moto main non-free contrib
+		EOF
+	fi
+}
 
-read -p "Enter your choice [1-3] " index
+function fixit(){
+	printf "[+] Cleaning the local repository of retrieved package files..\n"
+	apt-get clean
+	printf "[+] Fixing the errors..\n"
+	apt-get -f install
+}
+	
+VERSION=$(grep VERSION_ID /etc/os-release | cut -d '"' -f2)
 
-if [ "$index" = "1" ] || [ "$index" = "" ]
+if [[ $VERSION == 1.[0-9] ]]
 then
-    sudo rm -r /etc/apt/sources.list &> /dev/null
-    The_Kali_Rolling_Repository
-    echo "[+] Updating repos please wait..."
-    sudo apt-get clean 
-    echo "[+] Cleaning the local repository of retrieved package files..."
-    sudo apt-get -f install
-    echo "[+] Fixing the errors..."
-    
-elif [ $index -eq 2 ]
+	kali-moto
+	fixit
+elif [[ $VERSION == 2.[0-9] ]]
 then
-    sudo rm -r /etc/apt/sources.list &> /dev/null 
-    Retired_Kali_sana_2_0_Repositories
-    echo "[+] Updating repos please wait..."
-    sudo apt-get clean
-    echo "[+] Cleaning the local repository of retrieved package files..."
-    sudo apt-get -f install
-    echo "[+] Fixing the errors..."
-    
-elif [ $index -eq 3 ]
+	kali-sana
+	fixit
+elif [[ $VERSION == 201[0-9].[0-9] ]]
 then
-    sudo rm -r /etc/apt/sources.list &> /dev/null 
-    Retired_Kali_moto_1_0_Repositories
-    echo "[+] Updating repos please wait..."
-    sudo apt-get clean
-    echo "[+] Cleaning the local repository of retrieved package files..."
-    sudo apt-get -f install
-    echo "[+] Fixing the errors..."
- 
-else
-    echo "[!] You entered an wrong input try again..."
-fi
-
-file="/etc/apt/sources.list"
-if [ -f "$file" ]
-then
-echo "[+] Repos updated successfully"
-else
-    echo "[-] Repos not updated successfully if you not run script with root user then run script again with root power"
+	kali-rolling
+	fixit
 fi
